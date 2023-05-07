93dcd8119269ee9d227416f49075813fd757b3ba
Plb66 <126498700+Plb66@users.noreply.github.com>
2023-05-07 18:25:08 +0200

    dodan kalendar i navigacija za kalendar

--- Prog_in-servis_za_cisc/servis_za_cisc/src/pages/kalendarAdmin.vue
+++ Prog_in-servis_za_cisc/servis_za_cisc/src/pages/kalendarAdmin.vue
@@ -1,19 +1,91 @@
 <template>
-  <q-page padding class="flex flex-center">
-    <div class="q-gutter-md" style="max-width: 350px">
+<q-page padding class="flex-center full-height" style="height: 100%; width: 100%">
+    <div class="subcontent">
+    <navigation-bar
+      @today="onToday"
+      @prev="onPrev"
+      @next="onNext"
+    />
 
-      <div class="row justify-center q-pa-md">
-        <q-btn align: between unelevated color="primary" label="Potvrdi" text-align="center" />
-      </div>
-      <div class="row justify-center q-pa-md">
-        <q-btn align: between unelevated color="primary" @click="GoIzbornik" label="-->" text-align="center" />
+    <div class="row justify-center">
+      <div style="display: flex; max-width: 1200px; width: 100%; height: 400px;">
+        <q-calendar-day
+          ref="calendar"
+          v-model="selectedDate"
+          view="week"
+          animated
+          bordered
+          @change="onChange"
+          @moved="onMoved"
+          @click-date="onClickDate"
+          @click-time="onClickTime"
+          @click-interval="onClickInterval"
+          @click-head-intervals="onClickHeadIntervals"
+          @click-head-day="onClickHeadDay"
+        >
+          <template #head-day-event="{ scope }">
+            <!-- do every other slot -->
+            <div class="fit row justify-center">
+              <span>{{ scope.timestamp.weekday % 2 ? scope.timestamp.date : undefined }}</span>
+            </div>
+          </template>
+        </q-calendar-day>
       </div>
     </div>
-  </q-page>
+  </div>
+</q-page>
 </template>
 
 
-<script setup>
-
-
+<script>
+import { QCalendarDay, today } from '@quasar/quasar-ui-qcalendar/src/index.js'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarVariables.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarTransitions.sass'
+import '@quasar/quasar-ui-qcalendar/src/QCalendarDay.sass'
+import { defineComponent } from 'vue'
+import NavigationBar from '../components/NavigationBar.vue'
+export default defineComponent({
+  name: 'WeekSlotHeadDayEvent',
+  components: {
+    NavigationBar,
+    QCalendarDay
+  },
+  data () {
+    return {
+      selectedDate: today()
+    }
+  },
+  methods: {
+    onToday () {
+      this.$refs.calendar.moveToToday()
+    },
+    onPrev () {
+      this.$refs.calendar.prev()
+    },
+    onNext () {
+      this.$refs.calendar.next()
+    },
+    onMoved (data) {
+      console.log('onMoved', data)
+    },
+    onChange (data) {
+      console.log('onChange', data)
+    },
+    onClickDate (data) {
+      console.log('onClickDate', data)
+    },
+    onClickTime (data) {
+      console.log('onClickTime', data)
+    },
+    onClickInterval (data) {
+      console.log('onClickInterval', data)
+    },
+    onClickHeadIntervals (data) {
+      console.log('onClickHeadIntervals', data)
+    },
+    onClickHeadDay (data) {
+      console.log('onClickHeadDay', data)
+    }
+  }
+})
 </script>
